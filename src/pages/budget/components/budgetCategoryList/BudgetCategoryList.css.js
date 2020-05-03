3e6968622d0346626c9d0c8624956e643782a84c
Lutencjusz <lutencjusz@gmail.com>
2020-05-03 21:40:57 +0200

    $1

--- src/pages/budget/components/budgetCategoryList/BudgetCategoryList.css.js
+++ src/pages/budget/components/budgetCategoryList/BudgetCategoryList.css.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
-export const ParentCategory = styled.div`
+export const Category = styled.div`
     border: 1px solid ${({theme}) => theme.colors.grey.dark};
     padding: ${({ theme}) => theme.spacing.xs};
     display: flex;
     justify-content: space-between;
 `
+
+export const ParentCategory = styled(Category)`
+    background-color: ${({ theme }) => theme.colors.grey.normal};
+`
+
 
 export const CategoryItem = styled.div`
     background-color: ${({ theme }) => theme.colors.grey.light};
